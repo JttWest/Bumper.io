0e279bc3a3671a748b536f3d700a46f5c4b048b3
JttWest <johnson.h.2010@hotmail.com>
2017-07-17 01:03:32 -0700

    init

---
+++ client/js/app.js
@@ -0,0 +1,334 @@
+require('../css/app.css')
+const configs = require('../../game-configs.json')
+
+class Player {
+  constructor(x, y, h, w) {
+    this.x = x
+    this.y = y
+    this.h = h
+    this.w = w
+  }
+}
+const player = new Player(395, 295, 10, 10)
+// const ws = new WebSocket('ws://localhost:3000')
+const ws = new WebSocket('ws://top-down-shooter.herokuapp.com')
+
+let bullets = []
+let otherPlayers = []
+
+ws.onopen = () => {
+  // Web Socket is connected, send data using send()
+}
+
+ws.onmessage = (evt) => {
+  const payload = JSON.parse(evt.data)
+
+  player.x = payload.player.x
+  player.y = payload.player.y
+
+  bullets = payload.bullets
+
+  otherPlayers = payload.otherPlayers
+}
+
+ws.onclose = function () {
+  // websocket is closed.
+}
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const keys = {}
+
+canvas.addEventListener('keydown', (e) => {
+  keys[e.keyCode] = true
+})
+
+canvas.addEventListener('keyup', (e) => {
+  keys[e.keyCode] = false
+})
+
+function sendMoveData() {
+  const movementData = {
+    type: 'move',
+    data: { left: false, right: false, up: false, down: false }
+  }
+
+  if (keys[87])
+    movementData.data.up = true
+
+  if (keys[83])
+    movementData.data.down = true
+
+  if (keys[65])
+    movementData.data.left = true
+
+  if (keys[68])
+    movementData.data.right = true
+
+  ws.send(JSON.stringify(movementData))
+}
+
+setInterval(sendMoveData, 100)
+
+function bulletsDraw() {
+  bullets.forEach((b) => {
+    ctx.beginPath()
+    ctx.fillStyle = configs.client.bulletColor
+    ctx.rect(b.position.x, b.position.y, 5, 5)
+    ctx.fill()
+  })
+}
+
+function drawPlayer(color, x, y) {
+  ctx.beginPath()
+  ctx.fillStyle = color
+  ctx.strokeStyle = 'black'
+  ctx.rect(x, y, player.w, player.h)
+  ctx.lineWidth = 1
+  ctx.stroke()
+  ctx.fill()
+}
+
+function mainDraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  otherPlayers.forEach((p) => {
+    drawPlayer(configs.client.otherPlayersColor, p.x, p.y)
+  })
+
+  bulletsDraw()
+
+  drawPlayer(configs.client.playerColor, player.x, player.y)
+
+  requestAnimationFrame(mainDraw)
+}
+
+mainDraw()
+
+let mouseX
+let mouseY
+
+function mouseMove(e) {
+  mouseX = e.offsetX
+  mouseY = e.offsetY
+}
+
+function sendFireData(angle) {
+  const fireBulletData = {
+    type: 'fire',
+    data: { angle }
+  }
+
+  ws.send(JSON.stringify(fireBulletData))
+}
+
+canvas.addEventListener('mousemove', mouseMove)
+canvas.addEventListener('click', () => {
+  const deltaX = mouseX - player.x
+  const deltaY = mouseY - player.y
+  const fireAngle = Math.atan2(deltaY, deltaX)
+  sendFireData(fireAngle)
+})
+
+/*
+window.onload = function () {
+  // ---------------- init -------------------------------------- //
+  // canvas init
+  let canvas = document.getElementById('canvas')
+  canvas.width = 800
+  canvas.height = 600
+  let c = canvas.getContext('2d')
+  let WIDTH = canvas.width
+  let HEIGHT = canvas.height
+
+  // key press init
+  let keys = []
+
+  // player init
+  let player1 = new Player(395, 295, 10, 10)
+
+  // bullet init
+  let deltaX = 0
+  let deltaY = 0
+  let rotation = 0
+  let xtarget = 0
+  let ytarget = 0
+  let theBullets = []
+
+  // bad guy init
+  let theBadGuys = []
+  let spawnX
+  let spawnY
+
+  let mouseX
+  let mouseY
+
+  // ---------------- end init --------------------------------- //
+
+  function mainDraw() {
+    c.clearRect(0, 0, WIDTH, HEIGHT)
+
+    c.beginPath()
+    c.fillStyle = 'red'
+    c.strokeStyle = 'blue'
+    c.rect(player1.x, player1.y, player1.w, player1.h)
+    c.lineWidth = 1
+    c.stroke()
+    c.fill()
+
+    playerMove()
+
+    badGuysMove()
+    badGuysDraw()
+
+    bulletsMove()
+    bulletsDraw()
+
+    checkBulletHits()
+
+    if (Math.random() * 100 < 3) {
+      pushBadGuy()
+    }
+
+    removeOutOfBoundBullets()
+
+    requestAnimationFrame(mainDraw)
+  }
+
+  mainDraw()
+
+  canvas.addEventListener('keydown', (e) => {
+    keys[e.keyCode] = true
+  })
+
+  canvas.addEventListener('keyup', (e) => {
+    keys[e.keyCode] = false
+  })
+
+  function playerMove(e) {
+    if (keys[87]) {
+      player1.y -= 2
+    }
+    if (keys[83]) {
+      player1.y += 2
+    }
+    if (keys[65]) {
+      player1.x -= 2
+    }
+    if (keys[68]) {
+      player1.x += 2
+    }
+    return false
+  }
+
+  function mouseMove(e) {
+    mouseX = e.offsetX
+    mouseY = e.offsetY
+  }
+
+  canvas.addEventListener('mousemove', mouseMove)
+  canvas.addEventListener('click', () => {
+    createBullet(mouseX, mouseY, player1.x, player1.y)
+  })
+
+  function createBullet(targetX, targetY, shooterX, shooterY) {
+    deltaX = targetX - shooterX
+    deltaY = targetY - shooterY
+    rotation = Math.atan2(deltaY, deltaX)
+    xtarget = Math.cos(rotation)
+    ytarget = Math.sin(rotation)
+
+    theBullets.push({
+      active: true,
+      x: shooterX,
+      y: shooterY,
+      speed: 10,
+      xtarget,
+      ytarget,
+      w: 3,
+      h: 3,
+      color: 'black',
+      angle: rotation
+    })
+  }
+
+  function bulletsMove() {
+    theBullets.forEach((i) => {
+      i.x += i.xtarget * i.speed
+      i.y += i.ytarget * i.speed
+    })
+  }
+
+  function bulletsDraw() {
+    theBullets.forEach((i) => {
+      c.beginPath()
+      c.fillStyle = 'black'
+      c.rect(i.x, i.y, i.w, i.h)
+      c.fill()
+    })
+  }
+
+  function removeOutOfBoundBullets() {
+    for (let i = 0; i < theBullets.length; ++i) {
+      let bullet = theBullets[i]
+
+      if (bullet.x > WIDTH || bullet.x < 0 || bullet.y < 0 || bullet.y > HEIGHT)
+        theBullets.splice(i, 1)
+    }
+  }
+
+  function pushBadGuy() {
+    if (Math.random() < 0.5) {
+      spawnX = Math.random() < 0.5 ? -11 : 801
+      spawnY = Math.random() * 600
+    } else {
+      spawnX = Math.random() * 800
+      spawnY = Math.random() < 0.5 ? -11 : 601
+    }
+
+    theBadGuys.push({
+      x: spawnX, y: spawnY, w: 10, h: 10, speed: Math.random()
+    })
+  }
+
+  function badGuysMove() {
+    theBadGuys.forEach((i, j) => {
+      if (i.x > player1.x) { i.x -= i.speed }
+      if (i.x < player1.x) { i.x += i.speed }
+      if (i.y > player1.y) { i.y -= i.speed }
+      if (i.y < player1.y) { i.y += i.speed }
+    })
+  }
+
+  function badGuysDraw() {
+    theBadGuys.forEach((i, j) => {
+      c.beginPath()
+      c.fillStyle = 'blue'
+      c.strokeStyle = 'red'
+      c.rect(i.x, i.y, i.w, i.h)
+      c.lineWidth = 1
+      c.stroke()
+      c.fill()
+    })
+  }
+
+  function collides(a, b) {
+    return a.x < b.x + b.w &&
+      a.x + a.w > b.x &&
+      a.y < b.y + b.h &&
+      a.y + a.h > b.y
+  }
+
+  function checkBulletHits() {
+    if (theBullets.length > 0 && theBadGuys.length > 0) {
+      for (let j = theBullets.length - 1; j >= 0; j--) {
+        for (let k = theBadGuys.length - 1; k >= 0; k--) {
+          if (collides(theBadGuys[k], theBullets[j])) {
+            theBadGuys.splice(k, 1)
+          }
+        }
+      }
+    }
+  }
+}
+*/
---
+++ server/server.js
@@ -0,0 +1,187 @@
+const express = require('express')
+const WebSocket = require('ws')
+const path = require('path')
+const configs = require('../game-configs.json')
+
+const PORT = process.env.PORT || 3000
+
+const server = express()
+  .use(express.static(path.join(__dirname, '../dist')))
+  // .get('/', (req, res) => {
+  //   res.sendFile('index.html', { root: './dist' })
+  // })
+  // .get('/bundle.js', (req, res) => {
+  //   res.sendFile('/bundle.js', { root: './dist' })
+  // })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const wss = new WebSocket.Server({ server })
+
+const players = []
+
+function Player(name, ws) {
+  this.name = name
+  this.position = { x: 395, y: 295 }
+  this.ammo = 0
+  this.movement = { left: false, right: false, up: false, down: false }
+  this.isAlive = true
+  this.health = 3
+  this.websocket = ws
+}
+
+/*
+Player object
+postion: { x: number, y: number }
+ammo: number
+movement: { left: boolean, right: boolean, up: boolean, down: boolean }
+isAlive: boolean
+health: number
+websocket: ws
+size: { width: number, height: number }
+*/
+
+const bullets = []
+
+function Bullet(originPosition, shootingAngle) {
+  this.position = Object.assign({}, originPosition)
+  this.velocity = { dx: Math.cos(shootingAngle), dy: Math.sin(shootingAngle) }
+  this.lifeSpan = 10
+}
+/*
+Bullet object
+postion: {x: number, y: number}
+targetPostion: {x: number, y: number}
+lifeSpan: number
+size: { width: number, height: number }
+*/
+
+/*
+ws
+onConnect: createPlayer(ws)
+onDisconnect: removePlayer()
+onMessage:
+  fire: createBullet()
+  move: updatePlayerLocation()
+*/
+
+// { left: boolean, right: boolean, up: boolean, down: boolean }
+function updatePlayerLocation(player, direction) {
+  const SPEED = 2
+
+  if (direction.left)
+    player.position.x -= SPEED
+
+  if (direction.right)
+    player.position.x += SPEED
+
+  if (direction.up)
+    player.position.y -= SPEED
+
+  if (direction.down)
+    player.position.y += SPEED
+}
+
+wss.on('connection', (ws) => {
+  const player = new Player('name1', ws)
+  players.push(player)
+
+  ws.on('message', (message) => {
+    // TODO: implement try/catch logic to prevent server terminating
+    // when payload is not valid JSON
+    try {
+      const payload = JSON.parse(message)
+
+      if (payload.type === 'move') {
+        updatePlayerLocation(player, payload.data)
+      } else if (payload.type === 'fire') {
+        bullets.push(new Bullet(player.position, payload.data.angle))
+      } else {
+        console.log(`Received invalid message type from client: ${payload.type}`)
+      }
+    } catch (e) {
+      console.log(e)
+      ws.terminate()
+      // TODO: remove player from players queue
+    }
+  })
+})
+
+function moveBullets() {
+  // move bullets
+  bullets.forEach((bullet) => {
+    // TODO: use unit x and y distance
+    bullet.position.x += bullet.velocity.dx * configs.client_server.bulletSpeed
+    bullet.position.y += bullet.velocity.dy * configs.client_server.bulletSpeed
+
+    bullet.lifeSpan--
+  })
+}
+
+function rectanglesCollides(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  )
+}
+
+function checkBulletHits() {
+  for (let i = players.length - 1; i >= 0; i--) {
+    const player = players[i]
+
+    for (let ii = bullets.length - 1; ii >= 0; ii--) {
+      const bullet = bullets[ii]
+
+      if (rectanglesCollides(player, bullet)) {
+        if (player.health > 0)
+          player.health--
+      }
+    }
+  }
+}
+
+function cleanBullets() {
+  // remove bullets with no lifeSpan left
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    if (bullets[i].lifeSpan <= 0)
+      bullets.splice(i, 1)
+  }
+}
+
+function gameLoop() {
+  // this is driven by client instead (for now)
+  // movePlayers()
+
+  moveBullets()
+  checkBulletHits()
+
+  // TODO
+  //cleanPlayers() // players are remove from array when ws is disconnected
+  //cleanBullets()
+}
+
+// run game loop
+setInterval(gameLoop, 300)
+
+function broadcastGameData() {
+  // let playersData
+
+  players.forEach((player, i) => {
+    if (player.websocket.readyState === WebSocket.OPEN) {
+      player.websocket.send(
+        JSON.stringify(
+          {
+            player: player.position,
+            bullets,
+            otherPlayers: players
+              .filter((p, ii) => i !== ii) // remove current player from other player array
+              .map(p => p.position)
+          }
+        ))
+    }
+  })
+}
+
+// broadcast game data
+setInterval(broadcastGameData, 100)
