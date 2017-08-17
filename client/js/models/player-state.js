c35212adc2674b37b9519cbc691068e637e05e9d
JttWest <johnson.h.2010@hotmail.com>
2017-08-17 00:15:52 -0700

    added player-state

---
+++ client/js/models/player-state.js
@@ -0,0 +1,29 @@
+const configs = require('../../../game-configs.json')
+
+module.exports = class PlayerState {
+  constructor(name) {
+    this.name = name
+    this.position = null
+    this.snapshotQueue = []
+  }
+
+  processSnapshots(snapshots) {
+    snapshots.forEach((snapshot) => {
+      this.processPlayerMove(snapshot.movement)
+    })
+  }
+
+  processPlayerMove(movement) {
+    if (movement.left)
+      this.position.x -= configs.shared.playerSpeed
+
+    if (movement.right)
+      this.position.x += configs.shared.playerSpeed
+
+    if (movement.up)
+      this.position.y -= configs.shared.playerSpeed
+
+    if (movement.down)
+      this.position.y += configs.shared.playerSpeed
+  }
+}
