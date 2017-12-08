21e2f88a0648097fa9ba094edc3d600755a1a0ab
Johnson Huang <johnson.h.2010@hotmail.com>
2017-12-08 14:55:51 -0800

    removed used files

--- client/js/models/player.js
+++
@@ -1,8 +0,0 @@
-const Coord = require('../../../shared/models/coord');
-
-module.exports = class Player {
-  constructor(x, y) {
-    this.position = new Coord(x, y);
-    this.actions = {};
-  }
-};
