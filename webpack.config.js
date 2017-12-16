be71159bd34146319505a728b3a0317e1c80c2f8
Johnson Huang <johnson.h.2010@hotmail.com>
2017-12-15 22:01:45 -0800

    preparing for react/jsx

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=100000' }
     ]
