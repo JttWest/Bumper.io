f42d3c6f046ff969fc7f50bb46a14cbdac5d9fba
Johnson Huang <johnson.h.2010@hotmail.com>
2017-12-16 15:52:54 -0800

    sort leaderboard

--- client/js/ui/leaderboard.js
+++ client/js/ui/leaderboard.js
@@ -6,6 +6,15 @@ const updateLeaderboard = () => {
   const gameState = global.get('gameState');
 
   const scoreboardElements = Object.values(gameState.players)
+    .sort((p1, p2) => {
+      // order the players in reverse (most points is first)
+      if (p1.points > p2.points)
+        return -1;
+      else if (p1.points < p2.points)
+        return 1;
+
+      return 0;
+    })
     .map(player => `<li>${player.name}: ${player.points}</li>`);
 
   $('#leaderboardContainer').html(scoreboardElements.join(''));
