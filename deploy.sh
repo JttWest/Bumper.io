74213eac34d637a9272fd89ca7465ae0914487ca
Johnson Huang <johnson.h.2010@hotmail.com>
2018-01-02 21:20:48 -0800

    refractor

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ git reset --hard origin/master
 # set port to 80
 sed -i -E 's/port: [0-9]+/port: 80/g' app-configs.js
 
-npm install --only=production
+npm install
 npm run build:prod
 
-pm2 delete all
+pm2 delete all || true # allow this to fail; since app might not be running
 pm2 start server/server.js
 
 echo Successfully deployed!
